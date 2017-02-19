32b53aa632e8bf8c21601ed97a36834bc4bebb68
Subhaswini <cjsubbu@gmail.com>
2017-02-19 10:49:42 +0530

    3rd commit

--- app/src/main/java/com/example/ctadmin/quizapp/MainActivity.java
+++ app/src/main/java/com/example/ctadmin/quizapp/MainActivity.java
@@ -2,13 +2,13 @@ package com.example.ctadmin.quizapp;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+        public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //klgdrlnhjhjljgfg
+
+       }
     }
-}
+
